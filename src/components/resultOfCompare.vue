b35017761ce238b0ff312a3f462b0ff2584b3e65
KRYLOV <alexkrylov.2003@yandex.ru>
2022-11-30 16:59:06 +0300

    Нахождение всех различающихся строк в первой и второй таблицах

--- src/components/resultOfCompare.vue
+++ src/components/resultOfCompare.vue
@@ -1,6 +1,9 @@
 <template>
      <div class="res-blck">
-          <span v-for="item in nesovpalo_1" :key="item">
+          <span v-for="item in nonMatched_1" :key="item">
+            {{item}}
+          </span><br>
+          <span v-for="item in nonMatched_2" :key="item">
             {{item}}
           </span>
      </div>
@@ -15,7 +18,8 @@ export default {
      secondTable: this.$store.getters.SECOND_TABLE,
      selected_1: this.$store.getters.SELECTED_PARAMS_1,
      selected_2: this.$store.getters.SELECTED_PARAMS_2,
-     nesovpalo_1: [],
+     nonMatched_1: '',
+     nonMatched_2: '',
     }
   },
 
@@ -26,9 +30,8 @@ export default {
 
   methods: {
      toGetSelectedData() {
-     //извлеяение данных нужный параметров первой таблицы
+     //извлеяение данных нужных параметров первой таблицы
       let firstTable = this.firstTable.slice()
-
 
         let itter = Object.keys(firstTable[0]).map(function(item) { 
           return item.trim()
@@ -44,10 +47,8 @@ export default {
         });
         console.log('1', firstTable)
 
-        //извлеяение данных нужный параметров второй таблицы
+        //извлеяение данных нужных параметров второй таблицы
         let secondTable = this.secondTable.slice()
-
-
 
         let itter2 = Object.keys(secondTable[0]).map(function(item) { 
           return item.trim()
@@ -70,21 +71,53 @@ export default {
        
       let table_2 = this.secondTable.slice()
  
-        
+      let matched_1 = []
+      let nonMatched_1 = []
 
-        table_1.forEach(row => {
-          table_2.forEach(row2 => {
+      let matched_2 = []
+      let nonMatched_2 = []
+
+        table_1.forEach((row, index) => {
+          table_2.forEach((row2, index2) => {
             if(lodash.isEqual(row, row2)) {
-              console.log('Совпало', row, row2)
-             // this.sovpalo.push(row)
+              
+              table_1[index2]
+              matched_1.push(table_1[index])
+             //console.log('+', matched)
+             nonMatched_1 = table_1.filter(val => !matched_1.includes(val))
+             console.log('t323', nonMatched_1)
+             
             } else {
-              console.log('НЕСовпало', row, row2)
-              this.nesovpalo_1.push(row)
-              this.nesovpalo_1 = lodash.uniq(this.nesovpalo_1);
+              
+              console.log('НЕСовпало', table_1)
+
             }
           })
         }); 
+        this.nonMatched_1 = nonMatched_1
+              console.log(this.nonMatched_1)
 
+
+
+        table_2.forEach((row2, index2) => {
+          table_1.forEach((row, index) => {
+            if(lodash.isEqual(row2, row)) {
+              
+              table_2[index]
+              matched_2.push(table_2[index2])
+             //console.log('+', matched)
+             nonMatched_2 = table_2.filter(val => !matched_2.includes(val))
+             console.log('t555', nonMatched_2)
+             
+            } else {
+              
+              console.log('НЕСовпало', table_2)
+
+            }
+          })
+        }); 
+        this.nonMatched_2 = nonMatched_2
+              console.log(this.nonMatched_2)
     
      }
   }
