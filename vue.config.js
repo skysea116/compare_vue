18f12e26ea962b0938d018cbdbf530621d882fbb
KRYLOV <alexkrylov.2003@yandex.ru>
2022-12-18 20:08:14 +0300

    Промежуточное изменение

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-module.exports = {
-  publicPath: '/compare_vue/'
-}
+// module.exports = {
+//   publicPath: '/compare_vue/'
+// }cd
